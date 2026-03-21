d3f76588af59e58a19667a1ccaae444136ab1173
jahongirurinbayev292-droid <jahongirurinbayev292@gmail.com>
2026-03-21 19:41:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-// config.js - Sening shaxsiy Firebase ma'lumotlaring
+// ============ FIREBASE SOZLAMALARI ============
 const firebaseConfig = {
-  apiKey: "AIzaSyCJN4P9S4QgFWD5FzfFCZfDZlaCYj8SGYk",
-  authDomain: "pekin-tavor.firebaseapp.com",
-  databaseURL: "https://pekin-tavor-default-rtdb.firebaseio.com",
-  projectId: "pekin-tavor",
-  storageBucket: "pekin-tavor.firebasestorage.app",
-  messagingSenderId: "835006337740",
-  appId: "1:835006337740:web:12cec1149d6881e8a8e869",
-  measurementId: "G-F92YS7RTB9"
+    apiKey: "AIzaSyB7wXxXxXxXxXxXxXxXxXxX",
+    authDomain: "pekin-tavor.firebaseapp.com",
+    databaseURL: "https://pekin-tavor-default-rtdb.firebaseio.com",
+    projectId: "pekin-tavor",
+    storageBucket: "pekin-tavor.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef123456"
 };
 
 const ADMIN_PASSWORD = "4321";
