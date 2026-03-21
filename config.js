0f4d2cb5c26049f686a07b2e042e9629a7f42c6d
jahongirurinbayev292-droid <jahongirurinbayev292@gmail.com>
2026-03-21 18:33:01 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js - Sening shaxsiy Firebase ma'lumotlaring
+const firebaseConfig = {
+  apiKey: "AIzaSyCJN4P9S4QgFWD5FzfFCZfDZlaCYj8SGYk",
+  authDomain: "pekin-tavor.firebaseapp.com",
+  databaseURL: "https://pekin-tavor-default-rtdb.firebaseio.com",
+  projectId: "pekin-tavor",
+  storageBucket: "pekin-tavor.firebasestorage.app",
+  messagingSenderId: "835006337740",
+  appId: "1:835006337740:web:12cec1149d6881e8a8e869",
+  measurementId: "G-F92YS7RTB9"
+};
+
+const ADMIN_PASSWORD = "4321";
