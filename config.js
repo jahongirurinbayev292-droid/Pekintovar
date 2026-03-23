eac4b45785141851c14cb109a96185ef4970d4de
jahongirurinbayev292-droid <jahongirurinbayev292@gmail.com>
2026-03-24 00:21:22 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyB7wXxXxXxXxXxXxXxXxXxX",
-    authDomain: "pekin-tavor.firebaseapp.com",
-    databaseURL: "https://pekin-tavor-default-rtdb.firebaseio.com",
-    projectId: "pekin-tavor",
-    storageBucket: "pekin-tavor.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef123456"
-};
 
-const ADMIN_PASSWORD = "4321";
